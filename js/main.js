51b0b166ff87040dc84d506b21d8f5c794adb39c
jtdjdu6868 <99466048+jtdjdu6868@users.noreply.github.com>
2023-06-18 16:35:12 +0800

    update apperance

--- js/main.js
+++ js/main.js
@@ -12,3 +12,7 @@ function copy_output()
     output_element.select();
     document.execCommand("copy");
 }
+window.onload = function()
+{
+    document.getElementById("markdown_input").value = "# Heading 1\r\n\r\n## Heading 2\r\n\r\n### Heading 3\r\n\r\nhr\r\n--------\r\n\r\n**Bold Text**\r\n\r\n*Italic Text*\r\n\r\n***Bold and Italic Text***\r\n\r\n[Link to bahamut](https://www.gamer.com.tw)\r\n\r\n![bahamut Post](https://i2.bahamut.com.tw/forum/post_advocacy.png)\r\n\r\n> Blockquote\r\n\r\n- List Item 1\r\n- List Item 2\r\n    - Sublist Item 1\r\n    - Sublist Item 2\r\n- List Item 3\r\n\r\n1. Ordered Item 1\r\n2. Ordered Item 2\r\n    1. Sublist Item A\r\n    2. Sublist Item B\r\n3. Ordered Item 3\r\n\r\nThis is `Inline code` in the line\r\n```python\r\n# Code block\r\nprint('Hello World!)\r\n```";
+}
